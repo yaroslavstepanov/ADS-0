8cd6d79a8d819ffb68f1b39e62b7e65d7846c560
yaroslavstepanov <75724049+yaroslavstepanov@users.noreply.github.com>
2021-02-25 09:26:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+      while(true)
+  {
+    a = a%b;
+    if(a==0)
+    {
+      return b;
+    }
+    b = b%a;
+    if(b==0)
+    {
+      return a;
+    }
+  }
 }
